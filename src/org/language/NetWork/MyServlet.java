e07980c81de483ff81bcd915b648f11828384f44
zlx <2428784410@qq.com>
2021-12-15 17:13:03 +0800

    tomcatdemo

---
+++ src/org/language/NetWork/MyServlet.java
@@ -0,0 +1,23 @@
+package org.language.NetWork;
+
+import org.language.MyTomcat.MyHttpServlet;
+import org.language.MyTomcat.MyResponse;
+import org.language.MyTomcat.MyResquest;
+
+import java.io.IOException;
+
+/**
+ * @author zlx <zhulx3@>
+ * Creat on 2021-12-14
+ */
+public class MyServlet extends MyHttpServlet {
+    @Override
+    public void doGet(MyResquest resquest, MyResponse response) throws IOException {
+
+    }
+
+    @Override
+    public void doPost(MyResquest resquest, MyResponse response) throws IOException {
+
+    }
+}
